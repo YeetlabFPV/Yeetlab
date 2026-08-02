2808cbb67b3275b77a37012b2a37e08f75a7b0ee
YeetlabFPV <tmw.fpv@gmail.com>
2026-08-02 15:31:58 +0200

    Add community gallery photo

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ async function loadCommunityGallery() {
   }
 
   try {
-    const response = await fetch("assets/community/gallery.json?v=20260801-gallery-masonry-23", {
+    const response = await fetch("assets/community/gallery.json?v=20260802-gallery-masonry-24", {
       cache: "no-store",
     });
 
